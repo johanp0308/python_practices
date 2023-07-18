f8ea7f0f5d7ffae6285e7c22e6c97011f02411d5
Johan Pachon <sebastian-johan1@hotmail.com>
2023-07-18 16:59:27 -0500

    Some files were moved, a folder was created to organize. Also added a file with new exercises

--- lesson/lesson2.py
+++ lesson/lesson2.py
@@ -1,1 +1,8 @@
 import string
+
+class animal:
+    def run(self):
+        print("Running...")
+    
+    def eat(self):
+        print("Eating...")
---
+++ practices/practices-18-07-23.py
@@ -0,0 +1,73 @@
+# 1. Escribir un programa que guarde en una variable el diccionario 
+# {'Euro':'€', 'Dollar':'$', 'Yen':'¥'}, pregunte al usuario por 
+# una divisa y muestre su símbolo o un mensaje de aviso si la 
+# divisa no está en el diccionario.
+
+# 2. Escribir un programa que pregunte al usuario su nombre, 
+# edad, dirección y teléfono y lo guarde en un diccionario. 
+# Después debe mostrar por pantalla el mensaje <nombre> tiene 
+# <edad> años, vive en <dirección> y su número de teléfono 
+# es <teléfono>.
+
+# 3. Escribir un programa que guarde en un diccionario los 
+# precios de las verduras de la tabla, pregunte al usuario por 
+# una verdura, un número de kilos y muestre por pantalla el 
+# precio a pagar. Si la fruta no está en el diccionario debe 
+# mostrar un mensaje informando de ello.
+
+
+# Verdura               Precio (Kg)
+# Brócoli                2500 COP
+# Pimentón           1250 COP
+# Arveja                 3500 COP
+
+# 4. Escribir un programa que cree un diccionario vacío y 
+# lo vaya llenado con información sobre una persona (por 
+# ejemplo nombre, edad, sexo, teléfono, correo 
+# electrónico, etc.) que se le pida al usuario. Cada 
+# vez que se añada un nuevo dato debe imprimirse el 
+# contenido del diccionario.
+
+# 5. Escribir un programa que cree un diccionario 
+# simulando una cesta de la compra. El programa debe 
+# preguntar el artículo y su precio y añadir el par 
+# al diccionario, hasta que el usuario decida terminar. 
+# Después se debe mostrar por pantalla la lista de la 
+# compra y el coste total
+
+# 6. Escribir un programa que gestione las facturas 
+# pendientes de cobro de una empresa. Las facturas 
+# se almacenarán en un diccionario donde la clave de 
+# cada factura será el número de factura y el valor 
+# el coste de la factura. El programa debe preguntar 
+# al usuario si quiere añadir una nueva factura, pagar 
+# una existente o terminar. Si desea añadir una nueva 
+# factura se preguntará por el número de factura y su 
+# coste y se añadirá al diccionario. Si se desea pagar
+# una factura se preguntará por el número de factura 
+# y se eliminará del diccionario. Después de cada 
+# operación el programa debe mostrar por pantalla la 
+# cantidad cobrada hasta el momento y la cantidad pendiente de cobro.
+
+# 7. Escribir un programa que permita gestionar la 
+# base de datos de clientes de una empresa. Los 
+# clientes se guardarán en un diccionario en el que 
+# la clave de cada cliente será su NIF, y el valor 
+# será otro diccionario con los datos del cliente 
+# (nombre, dirección, teléfono, correo, preferente), 
+# donde preferente tendrá el valor True si se 
+# trata de un cliente preferente. El programa debe 
+# preguntar al usuario por una opción del siguiente 
+# menú: (1) Añadir cliente, (2) Eliminar cliente, 
+# (3) Mostrar cliente, (4) Listar todos los 
+# clientes, (5) Listar clientes preferentes, (6) 
+# Terminar. En función de la opción elegida el 
+# programa tendrá que hacer lo siguiente:Preguntar 
+# los datos del cliente, crear un diccionario con 
+# los datos y añadirlo a la base de datos.Preguntar 
+# por el NIF del cliente y eliminar sus datos de 
+# la base de datos.Preguntar por el NIF del cliente 
+# y mostrar sus datos.Mostrar lista de todos los 
+# clientes de la base datos con su NIF y nombre.
+# Mostrar la lista de clientes preferentes de la 
+# base de datos con su NIF y nombre.Terminar el programa.
